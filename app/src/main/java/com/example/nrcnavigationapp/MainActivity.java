eaac8acc3a2f48160f202ec0be68b0ae6a07302c
Developer <dev@hostelbook.local>
2026-07-11 10:24:50 +0200

    Complete Phase 1: Display OpenStreetMap

---
+++ app/src/main/java/com/example/nrcnavigationapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.nrcnavigationapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapController;
+import org.osmdroid.util.GeoPoint;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize OSMDroid
+        Configuration.getInstance().load(
+                getApplicationContext(),
+                getSharedPreferences("osmdroid", MODE_PRIVATE)
+        );
+
+        setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+
+        map.setMultiTouchControls(true);
+
+        org.osmdroid.api.IMapController mapController = map.getController();
+        mapController.setZoom(18.0);
+
+        GeoPoint startPoint = new GeoPoint(-14.02, 33.67);
+
+        mapController.setCenter(startPoint);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+}
